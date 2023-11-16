caf7df825d96a5eef223fa488ba0027fc972e4f0
pardhiv9 <113880142+pardhiv9@users.noreply.github.com>
2023-11-16 19:53:02 +0530

    Delete 3) job_ascending.py

--- 3) job_ascending.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-data = pd.read_csv("C:\\Users\\USER\\OneDrive\\Documents\\job_title_ascen.csv")
-sorted_column = data.sort_values(by="JOB_TITLE",ascending=False)
-print(sorted_column)
-
